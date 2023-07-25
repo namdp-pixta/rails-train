c100a4052f16dd8cb0196ba8e22de676d3c51ac4
namdp-pixta <nam.dangphuong@pixta.co.jp>
2023-07-25 16:56:34 +0700

    Finish user edit, update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,11 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(email: params[:session][:email].downcase)
     if @user&.authenticate(params[:session][:password]) # this is like ? operator in typescript
+      forwarding_url = session[:forwarding_url]
       reset_session
       params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
       login(@user)
-      redirect_to @user
+      redirect_to forwarding_url || @user
     else
       flash.now[:danger] = 'Invalid email or password'
       render 'new', status: :unprocessable_entity
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root "static_pages#home"
-  get "/help", to: "static_pages#help"
-  get "/about", to: "static_pages#about"
-  get '/signup', to: "users#new"
+  root 'static_pages#home'
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
+  get '/signup', to: 'users#new'
 
   resources :users
-  get "/login", to: "sessions#new"
-  post "/login", to: "sessions#create"
-  delete "/logout", to: "sessions#destroy"
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
